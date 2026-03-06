50cd532a29cdd6f26748e1b1f8b63c613980831b
anu1964 <anukn2006@gmail.com>
2026-03-06 13:30:56 +0530

    final: all files ready

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/safecampus/',
+  base: '/',
+  server: {
+    port: 5173,
+    host: 'localhost',
+  },
 })
